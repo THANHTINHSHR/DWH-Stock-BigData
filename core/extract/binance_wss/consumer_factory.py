6f8aad453e429882db23f1319c63b9e06b09109c
THANHTINHSHR <thanhtinh14.06.1998@gmail.com>
2025-04-07 03:06:39 +0700

    refactor: clean up imports and improve code organization in consumer modules

--- core/extract/binance_wss/consumer_factory.py
+++ core/extract/binance_wss/consumer_factory.py
@@ -10,13 +10,14 @@ class ConsumerFactory:
         self.BOOTSTRAP_SERVERS = os.getenv("BOOTSTRAP_SERVERS")
         self.BINANCE_TOPIC = os.getenv("BINANCE_TOPIC")
 
-    def create_consumer(self, stream_type):
+    def create_consumer(self, symbol, stream_type):
         """Create a Kafka consumer for the given stream_type"""
         config = {
             "bootstrap.servers": self.BOOTSTRAP_SERVERS,
-            "group.id": stream_type,
+            "group.id": f"{symbol}_{stream_type}",
             "auto.offset.reset": "earliest",  # Change to latest if needed
         }
         consumer = Consumer(config)
-        consumer.subscribe([f"{self.BINANCE_TOPIC}_{stream_type}"])
+        consumer.subscribe([f"{self.BINANCE_TOPIC}_{symbol}"])
+        print(f"✅ Consumer subscribed for {self.BINANCE_TOPIC}_{symbol}")
         return consumer
--- core/extract/binance_wss/consumer_manager.py
+++ core/extract/binance_wss/consumer_manager.py
@@ -2,6 +2,8 @@ from confluent_kafka import Consumer, KafkaException, KafkaError
 from core.extract.binance_wss.consumer_factory import ConsumerFactory
 from confluent_kafka.schema_registry.avro import AvroDeserializer
 from confluent_kafka.serialization import SerializationContext, MessageField
+from core.extract.binance_wss.topic_creator import TopicCreator
+
 from core.extract.binance_wss.schema_avro.schema_registry_connector import (
     SchemaRegistryConnector,
 )
@@ -32,14 +34,14 @@ class ConsumerManager:
 
     def get_all_consumers(self):
         """Get all consumers"""
-        for stream_type in self.STREAM_TYPES:
-            if stream_type not in self.consumers:
-                self.consumers[stream_type] = ConsumerFactory().create_consumer(
-                    stream_type
+        for symbol in TopicCreator.TOPCOIN:
+            for stream_type in self.STREAM_TYPES:
+                self.consumers[f"{symbol}_{stream_type}"] = (
+                    ConsumerFactory().create_consumer(symbol, stream_type)
                 )
         return self.consumers
 
-    async def fetch_stream(self, stream_type, consumer):
+    async def fetch_stream(self, stream_type, consumer, symbol):
         try:
             while True:
                 msg = consumer.poll(timeout=1.0)
@@ -49,12 +51,11 @@ class ConsumerManager:
                 if msg.error():
                     raise KafkaException(msg.error())
                 else:
-                    deserializer = self.deserializers[stream_type]
+                    deserializer = self.deserializers[f"{symbol}_{stream_type}"]
                     ctx = SerializationContext(msg.topic(), MessageField.VALUE)
                     data = deserializer(msg.value(), ctx)
                     # Get coin symbol from the message
-                    coin = data.get("s")
-                    key = f"{stream_type}/{coin}/{msg.timestamp()[1]}.json"
+                    key = f"{symbol}/{stream_type}/{msg.timestamp()[1]}.json"
                     self.upload_to_s3(data, key)
 
         except KeyboardInterrupt:
@@ -65,12 +66,12 @@ class ConsumerManager:
 
     def deserialize(self):
         """Deserialize Avro data"""
-        for stream_type in self.STREAM_TYPES:
-            schema = self.registry.get_schema_by_name(f"{stream_type}")
-            schema_str = schema.schema.schema_str
-            deserializer = AvroDeserializer(self.registry.get_client(), schema_str)
-            self.deserializers[stream_type] = deserializer
-        pass
+        for symbol in TopicCreator.TOPCOIN:
+            for stream_type in self.STREAM_TYPES:
+                schema = self.registry.get_schema_by_name(f"{stream_type}")
+                schema_str = schema.schema.schema_str
+                deserializer = AvroDeserializer(self.registry.get_client(), schema_str)
+                self.deserializers[f"{symbol}_{stream_type}"] = deserializer
 
     def upload_to_s3(self, data, key):
         """Upload data to S3"""
@@ -92,10 +93,12 @@ class ConsumerManager:
     async def start_listen(self):
         tasks = []
         for key, consumer in self.consumers.items():
-            tasks.append(self.fetch_stream(key, consumer))
+            symbol, stream_type = key.split("_", 1)
+            tasks.append(self.fetch_stream(stream_type, consumer, symbol))
         await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
+    tc = TopicCreator()
     cm = ConsumerManager()
     asyncio.run(cm.start_listen())
