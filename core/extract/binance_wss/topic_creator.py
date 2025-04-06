d09b96bb2e0dc7a6ec7ee123e7c25ea9ad55e19b
THANHTINHSHR <thanhtinh14.06.1998@gmail.com>
2025-04-06 16:55:08 +0700

    feat: enhance TopicCreator to fetch top coins dynamically and adjust topic creation logic

--- core/extract/binance_wss/topic_creator.py
+++ core/extract/binance_wss/topic_creator.py
@@ -1,10 +1,11 @@
 from confluent_kafka.admin import AdminClient, NewTopic
 from confluent_kafka import Producer
 
-import os
+import os, requests, json
 from dotenv import load_dotenv
 
 load_dotenv()
+TOPCOIN = []
 
 
 class TopicCreator:
@@ -14,28 +15,36 @@ class TopicCreator:
         self.NUM_PARTITIONS = int(os.getenv("NUM_PARTITIONS", 1))
         self.BINANCE_TOPIC = os.getenv("BINANCE_TOPIC")
         self.STREAM_TYPES = os.getenv("STREAM_TYPES").split(",")
+        self.URL_TOP = os.getenv("URL_TOP")
+        self.LIMIT = int(os.getenv("LIMIT", 10))
+        # init value for TOPCOIN
+        self.get_top_coins()
+
+    def get_top_coins(self):
+        global TOPCOIN
+        response = requests.get(self.URL_TOP)
+        data = response.json()
+        top_coins = sorted(data, key=lambda x: float(x["quoteVolume"]), reverse=True)[
+            : self.LIMIT
+        ]
+        TOPCOIN = [coin["symbol"].lower() for coin in top_coins]
 
     def create_topic(self):
-        for stream_type in self.STREAM_TYPES:
-            topic_name = f"{self.BINANCE_TOPIC}_{stream_type}"
+        num_streams = len(self.STREAM_TYPES)
+        num_partitions = num_streams * len(TOPCOIN)
+        for symbol in TOPCOIN:
+            topic_name = f"{self.BINANCE_TOPIC}_{symbol}"  # ex : binance_btcusdt
             new_topic = NewTopic(
-                topic_name, num_partitions=self.NUM_PARTITIONS, replication_factor=1
+                topic_name, num_partitions=num_partitions, replication_factor=1
             )
             # Wait for the topic creation to complete
             futures = self.admin_client.create_topics([new_topic])
             for topic, future in futures.items():
                 try:
                     future.result()
-                    # self.send_dummy_message(topic_name)
                     print(f"✅ Topic '{topic}' created successfully")
                 except Exception as e:
                     print(f"❌ Failed to create topic '{topic}': {e}")
-
-    def send_dummy_message(self, topic_name):
-        p = Producer({"bootstrap.servers": self.BOOTSTRAP_SERVERS})
-        p.produce(topic_name, key="test", value="dummy")
-        p.flush()
-        print(f"✅ Dummy message sent to topic '{topic_name}'")
 
 
 if __name__ == "__main__":
