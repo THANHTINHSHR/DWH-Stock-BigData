dd266c1f48074484877b1eede2ea3f75ace0b10a
THANHTINHSHR <thanhtinh14.06.1998@gmail.com>
2025-04-07 12:03:35 +0700

    feat: Done Extract

--- core/extract/binance_wss/binance_extractor.py
+++ core/extract/binance_wss/binance_extractor.py
@@ -1,7 +1,9 @@
+import threading
+import asyncio
 from core.extract.binance_wss.producer_manager import ProducerManager
 from core.extract.binance_wss.consumer_manager import ConsumerManager
 from core.extract.binance_wss.topic_creator import TopicCreator
-import asyncio, os
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,15 +17,31 @@ class BinanceExtractor:
         self.producer_manager = ProducerManager()
         self.consumer_manager = ConsumerManager()
 
-    async def extract(self):
-        """Main method to run the extractor"""
-        # Run the consumer and producer tasks concurrently using asyncio.gather
-        await asyncio.gather(
-            self.consumer_manager.start_listen(), self.producer_manager.start_publish()
-        )
+    def start(self):
+        """Start the threads for producer and consumer."""
+        # Create and start the consumer thread
+        consumer_thread = threading.Thread(target=self.consumer_manager.start_listen)
+        consumer_thread.start()
+
+        # Create and start the producer thread with asyncio
+        producer_thread = threading.Thread(target=self.run_producer)
+        producer_thread.start()
+
+        # Wait for the threads to finish
+        consumer_thread.join()
+        producer_thread.join()
+
+    def run_producer(self):
+        """Run the producer async task in a new event loop."""
+        loop = asyncio.new_event_loop()  # Create a new event loop for this thread
+        asyncio.set_event_loop(loop)  # Set it as the current event loop
+        loop.run_until_complete(
+            self.producer_manager.start_publish()
+        )  # Run the async task
 
 
 if __name__ == "__main__":
+    # Create an instance of BinanceExtractor
     be = BinanceExtractor()
-    # Run the main async method using asyncio.run
-    asyncio.run(be.extract())
+    # Start the extractor (producer and consumer) in separate threads
+    be.start()
