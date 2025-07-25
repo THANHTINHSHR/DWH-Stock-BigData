c2e971ae98ee533351b955dfc57e02f344034dfa
Thanh Tính <Thanhtinh14.06.1998@gmail.com>
2025-07-25 23:58:25 +0700

    update: Change external

--- airflow/dags/trade_dag.py
+++ airflow/dags/trade_dag.py
@@ -16,6 +16,9 @@ with DAG(
     wait_for_init = ExternalTaskSensor(
         task_id="wait_for_project_init",
         external_dag_id="Project_init_dag",
+        external_task_id=None,
+        execution_date_fn=lambda _: None,
+        check_existence=False,
         mode="poke",
         timeout=600,
         poke_interval=30,
