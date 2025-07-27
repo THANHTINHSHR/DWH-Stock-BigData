39ea1bbf18d63d7b2bc54a0d8e012a30bca6ac3b
Thanh Tính <Thanhtinh14.06.1998@gmail.com>
2025-07-28 01:29:44 +0700

    logic

--- airflow/dags/trade_dag.py
+++ airflow/dags/trade_dag.py
@@ -1,12 +1,18 @@
 from airflow import DAG  # type: ignore
-from tasks.trade_pipline_task import TradePipelineTask
-from datetime import datetime, timedelta
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator  # type: ignore
+from airflow.operators.python import PythonOperator  # type: ignore
+from airflow.providers.standard.sensors.external_task import ExternalTaskSensor  # type: ignore
 from airflow.providers.cncf.kubernetes.secret import Secret  # type: ignore
+from airflow.models import DagRun  # type: ignore
+from airflow.utils.state import State  # type: ignore
+from datetime import datetime  # type: ignore
+from tasks.trade_pipline_task import TradePipelineTask
+
+# 🔧 DAG setup
 default_args = {
     "owner": "airflow",
     "start_date": datetime(2025, 7, 27, 3, 46, 37),
 }
+
 secret_keys = [
     # aiSpark
     "AI_SPARK_MODE", "AI_SPARK_LOCAL_DIR", "AI_SPARK_APP_NAME",
@@ -38,27 +44,54 @@ secret_keys = [
     "SEQUENCE_LENGTH", "PREDICTION_LENGTH", "NUM_EPOCHS"
 ]
 
-
 secrets = [
     Secret("env", key, secret="project-secret", key=key)
     for key in secret_keys
 ]
+
+
+def get_latest_success_execution_date(**kwargs):
+    session = kwargs['session']
+    dag_runs = session.query(DagRun).filter(
+        DagRun.dag_id == 'Project_init_dag',
+        DagRun.state == State.SUCCESS
+    ).order_by(DagRun.execution_date.desc()).limit(1).all()
+
+    if dag_runs:
+        latest_date = dag_runs[0].execution_date
+        kwargs['ti'].xcom_push(key='latest_execution_date', value=latest_date)
+    else:
+        raise ValueError("Không tìm thấy DAG thành công nào")
+
+
 with DAG(
     dag_id="Trade_dag",
     schedule=None,
     default_args=default_args,
-    catchup=False,
+    catchup=False
 ) as dag:
-    trigger_init_dag = TriggerDagRunOperator(
-        task_id='trigger_project_init',
-        trigger_dag_id='Project_init_dag',
-        wait_for_completion=True,
-        reset_dag_run=True,  # xóa run cũ nếu cùng execution_date
+
+    get_date_task = PythonOperator(
+        task_id='get_latest_execution_date',
+        python_callable=get_latest_success_execution_date,
+        provide_context=True
+    )
+
+    wait_for_init_task = ExternalTaskSensor(
+        task_id='Wait_For_Init_Task',
+        external_dag_id='Project_init_dag',
+        external_task_id='Project_init_Task',
+        execution_date="{{ task_instance.xcom_pull(task_ids='get_latest_execution_date', key='latest_execution_date') }}",
+        mode='poke',
+        timeout=600,
+        poke_interval=30,
         allowed_states=['success'],
         failed_states=['failed', 'skipped']
     )
 
     image = "dwh-stock-bigdata:3.0"
     trade_pipeline = TradePipelineTask(image, secrets=secrets).build()
-    trigger_init_dag >> trade_pipeline
+
+    get_date_task >> wait_for_init_task >> trade_pipeline
+
 globals()["Trade_dag"] = dag
