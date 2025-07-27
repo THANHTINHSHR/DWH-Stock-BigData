a736f2a79b95af167bea68cd2af6a04dea523822
Thanh Tính <Thanhtinh14.06.1998@gmail.com>
2025-07-27 14:57:56 +0700

    refactor: remove ExternalTaskSensor and simplify DAG structure

--- airflow/dags/ticker_dag.py
+++ airflow/dags/ticker_dag.py
@@ -51,17 +51,8 @@ with DAG(
     default_args=default_args,
     catchup=False,
 ) as dag:
-    wait_for_init = ExternalTaskSensor(
-        task_id="wait_for_project_init",
-        external_dag_id="Project_init_dag",
-        external_task_id="Project_init_Task",
-        execution_date_fn=lambda _: default_args["start_date"],
-        check_existence=False,
-        mode="poke",
-        timeout=600,
-        poke_interval=30,
-    )
+
     image = "dwh-stock-bigdata:3.0"
     ticker_pipeline = TickerPipelineTask(image, secrets=secrets).build()
-    wait_for_init >> ticker_pipeline
+    ticker_pipeline
 globals()["Ticker_dag"] = dag
