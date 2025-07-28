12ef6f7f0dc97011ca99c5655314b50b6b624452
Thanh Tính <Thanhtinh14.06.1998@gmail.com>
2025-07-28 16:04:41 +0700

    Update : Use SqlSensor

---
+++ airflow/dags/ticker_dag.py
@@ -0,0 +1,63 @@
+from airflow import DAG  # type: ignore
+from tasks.ticker_pipline_task import TickerPipelineTask
+from datetime import datetime, timedelta
+from airflow.providers.cncf.kubernetes.secret import Secret  # type: ignore
+from airflow.providers.common.sql.sensors.sql import SqlSensor  # type: ignore
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2025, 7, 27, 3, 46, 37),
+
+}
+
+secret_keys = [
+    # aiSpark
+    "AI_SPARK_MODE", "AI_SPARK_LOCAL_DIR", "AI_SPARK_APP_NAME",
+
+    # core
+    "SPARK_LOCAL_DIR", "SPARK_MODE",
+    "WSS_ENDPOINT", "URL_TOP", "LIMIT", "STREAM_TYPES",
+    "AWS_DEFAULT_REGION", "BUCKET_NAME", "ROOT_DB", "ATHENA_DB", "S3_STAGING_DIR",
+    "BOOTSTRAP_SERVERS", "BINANCE_TOPIC", "NUM_PARTITIONS",
+    "GRAFANA_URL", "GRAFANA_DB_URL",
+    "INFLUXDB_URL", "INFLUXDB_ORG", "INFLUXDB_BUCKET",
+    "SUPERSET_URL",
+
+    # aws
+    "AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "AWS_DEFAULT_REGION",
+
+    # grafana
+    "GRAFANA_KEY", "GRAFANA_ADMIN_USER", "GRAFANA_ADMIN_PASSWORD",
+
+    # influxdb
+    "INFLUXDB_TOKEN",
+
+    # superset
+    "SUPERSET_USERNAME", "SUPERSET_PASSWORD", "SUPERSET_SECRET_KEY",
+
+    # informer
+    "AI_APP_NAME", "REPARTITION", "TRAIN_RATIO", "VAL_RATIO",
+    "BATCH_SIZE", "N_DAYS_AGO", "MAX_DIRECTORIES",
+    "SEQUENCE_LENGTH", "PREDICTION_LENGTH", "NUM_EPOCHS"
+]
+
+
+secrets = [
+    Secret("env", key, secret="project-secret", key=key)
+    for key in secret_keys
+]
+with DAG(
+    dag_id="Ticker_dag",
+    schedule=None,
+    default_args=default_args,
+    catchup=False,
+) as dag:
+    check_task = SqlSensor(
+        task_id='check_project_init_success',
+        conn_id='airflow_db',
+        sql="SELECT COUNT(1) FROM dag_run WHERE dag_id = 'Project_init_dag' AND state = 'success'",
+    )
+    image = "dwh-stock-bigdata:3.0"
+    ticker_pipeline = TickerPipelineTask(image, secrets=secrets).build()
+    check_task >> ticker_pipeline
+globals()["Ticker_dag"] = dag
