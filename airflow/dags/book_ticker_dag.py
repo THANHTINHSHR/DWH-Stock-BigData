5142200fda5ff0d81dc232b7c8b54160a7efec1c
Thanh Tính <Thanhtinh14.06.1998@gmail.com>
2025-07-29 01:05:27 +0700

    test load secret

--- airflow/dags/book_ticker_dag.py
+++ airflow/dags/book_ticker_dag.py
@@ -2,26 +2,7 @@ from airflow import DAG  # type: ignore
 from datetime import datetime
 from airflow.providers.common.sql.sensors.sql import SqlSensor  # type: ignore
 from tasks.book_ticker_pipline_task import BookTickerPipelineTask
-
-
-def load_secrets():
-    from airflow.providers.cncf.kubernetes.secret import Secret  # type: ignore
-
-    secret_keys = [
-        "AI_SPARK_MODE", "AI_SPARK_LOCAL_DIR", "AI_SPARK_APP_NAME",
-        "SPARK_LOCAL_DIR", "SPARK_MODE", "WSS_ENDPOINT", "URL_TOP", "LIMIT",
-        "STREAM_TYPES", "AWS_DEFAULT_REGION", "BUCKET_NAME", "ROOT_DB", "ATHENA_DB",
-        "S3_STAGING_DIR", "BOOTSTRAP_SERVERS", "BINANCE_TOPIC", "NUM_PARTITIONS",
-        "GRAFANA_URL", "GRAFANA_DB_URL", "INFLUXDB_URL", "INFLUXDB_ORG", "INFLUXDB_BUCKET",
-        "SUPERSET_URL", "AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "GRAFANA_KEY",
-        "GRAFANA_ADMIN_USER", "GRAFANA_ADMIN_PASSWORD", "INFLUXDB_TOKEN",
-        "SUPERSET_USERNAME", "SUPERSET_PASSWORD", "SUPERSET_SECRET_KEY",
-        "AI_APP_NAME", "REPARTITION", "TRAIN_RATIO", "VAL_RATIO", "BATCH_SIZE",
-        "N_DAYS_AGO", "MAX_DIRECTORIES", "SEQUENCE_LENGTH", "PREDICTION_LENGTH", "NUM_EPOCHS"
-    ]
-
-    return [Secret("env", key, secret="project-secret", key=key) for key in secret_keys]
-
+from common.secret_loader import load_secrets
 
 default_args = {
     "owner": "airflow",
--- airflow/dags/project_init_dag.py
+++ airflow/dags/project_init_dag.py
@@ -2,48 +2,11 @@ from airflow import DAG  # type: ignore
 from tasks.project_init_task import ProjectInitTask
 from datetime import datetime
 from airflow.providers.cncf.kubernetes.secret import Secret  # type: ignore
-
+from common.secret_loader import load_secrets
 default_args = {
     "owner": "airflow",
     "start_date": datetime(2025, 7, 27, 3, 46, 37),
 }
-
-secret_keys = [
-    # aiSpark
-    "AI_SPARK_MODE", "AI_SPARK_LOCAL_DIR", "AI_SPARK_APP_NAME",
-
-    # core
-    "SPARK_LOCAL_DIR", "SPARK_MODE",
-    "WSS_ENDPOINT", "URL_TOP", "LIMIT", "STREAM_TYPES",
-    "AWS_DEFAULT_REGION", "BUCKET_NAME", "ROOT_DB", "ATHENA_DB", "S3_STAGING_DIR",
-    "BOOTSTRAP_SERVERS", "BINANCE_TOPIC", "NUM_PARTITIONS",
-    "GRAFANA_URL", "GRAFANA_DB_URL",
-    "INFLUXDB_URL", "INFLUXDB_ORG", "INFLUXDB_BUCKET",
-    "SUPERSET_URL",
-
-    # aws
-    "AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "AWS_DEFAULT_REGION",
-
-    # grafana
-    "GRAFANA_KEY", "GRAFANA_ADMIN_USER", "GRAFANA_ADMIN_PASSWORD",
-
-    # influxdb
-    "INFLUXDB_TOKEN",
-
-    # superset
-    "SUPERSET_USERNAME", "SUPERSET_PASSWORD", "SUPERSET_SECRET_KEY",
-
-    # informer
-    "AI_APP_NAME", "REPARTITION", "TRAIN_RATIO", "VAL_RATIO",
-    "BATCH_SIZE", "N_DAYS_AGO", "MAX_DIRECTORIES",
-    "SEQUENCE_LENGTH", "PREDICTION_LENGTH", "NUM_EPOCHS"
-]
-
-
-secrets = [
-    Secret("env", key, secret="project-secret", key=key)
-    for key in secret_keys
-]
 
 with DAG(
     dag_id="Project_init_dag",
@@ -51,6 +14,7 @@ with DAG(
     default_args=default_args,
     catchup=False,
 ) as dag:
+    secrets = load_secrets()
     image = "dwh-stock-bigdata:3.0"
     project_init_task = ProjectInitTask(image, secrets=secrets).build()
     project_init_task
