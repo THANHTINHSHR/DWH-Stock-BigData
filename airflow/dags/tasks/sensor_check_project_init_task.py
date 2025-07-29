8dd7babae5998c0ad4e00ff4c8bf80eb4e1d70c0
Thanh Tính <Thanhtinh14.06.1998@gmail.com>
2025-07-29 11:49:41 +0700

    test

--- airflow/dags/tasks/sensor_check_project_init_task.py
+++ airflow/dags/tasks/sensor_check_project_init_task.py
@@ -3,7 +3,7 @@ from airflow.providers.common.sql.sensors.sql import SqlSensor  # type: ignore
 
 class SensorCheckProjectInitTask:
     def __init__(self, state="Success"):
-        self.task_id = f"Check_Project_Init_{state}"
+        self.task_id = f"Project_Init_{state}"
         self.conn_id = 'airflow_db'
         self.state = state
         self.sql = f"SELECT COUNT(1) FROM dag_run WHERE dag_id = 'Project_init_dag' AND state = '{self.state}'"
